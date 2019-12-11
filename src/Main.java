9c1d33bc0b02d2c45a0a55f13f5b3631ad1e5c4f
e175767 <e175767@ie.u-ryukyu.ac.jp>
2019-12-11 21:28:39 +0900

    aaa

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "あああああ";
+        String str = "百二十三";
         int value;
         value = Integer.parseInt(str);
     }
