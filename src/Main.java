3cbe97cae7fe33f2b18f124a37b142513546e60a
e175767 <e175767@ie.u-ryukyu.ac.jp>
2019-12-11 21:24:37 +0900

    aaa

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "あああああ";
+        int value;
+        value = Integer.parseInt(str);
+    }
+}
